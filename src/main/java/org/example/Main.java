8679b2bf0f180116918e280e109b65885fe3735f
Luiz Fernando <luizfernandodemoura.2013@gmail.com>
2023-09-04 10:10:54 -0300

    Criando Projeto

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,5 +5,23 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o numero da Agência: ");
+        String agencia = scanner.nextLine();
+
+        System.out.println("Por favor, digite o numero da Conta: ");
+        int numeroConta = scanner.nextInt();
+
+        System.out.println("Por favor, digite o nome do Cliente:  ");
+        String nomeCliente =scanner.next();
+
+        System.out.println("Por favor, digite o saldo da Conta:  ");
+        double saldoConta = scanner.nextDouble();
+
+        scanner.close();
+
+        System.out.print("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, " +
+                "sua agência é " + agencia + ", conta " + numeroConta + " e seu saldo " + saldoConta +
+                " já está disponível para saque.");
     }
 }
